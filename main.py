f4346803a0266c983e2a15b843ef5a15cedac069
abrehan2 <rehannajam2@gmail.com>
2023-08-24 14:06:16 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+ # - import certain math functions:
+from math import *
+
+# print("Hello world")
+
+# VARIABLES AND DATATYPES -
+user_name = "Abdul Rehan"
+age = 21
+is_user = True
+
+# print(is_user)
+
+# WORKING WITH STRINGS -
+# phrase = "This is a car"
+# print(phrase[0]) # - grabbing characters from a string
+# print(phrase.index("a")) # - gives an index of a char
+# print(phrase.replace("car", "cycle"))
+
+# WORKING WITH NUMBERS -
+value = 5
+
+# print(str(value) + " is my age") # - convert a number to string
+# print(abs(value))
+# print(pow(value, 2)) # - value ^ 2
+# print(max(value, 2))
+# print(min(value, 2))
+# print(round(3.5))
+# print(floor(3.5)) # - it requires the use of the math library
+# print(ceil(3.5)) # - it requires the use of the math library
+# print(sqrt(2)) # - it requires the use of the math library
